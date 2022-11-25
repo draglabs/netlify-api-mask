3a338b54c8f249bfc45244f5857c21d86ab0564b
developermanishhub <developermanishhub@gmail.com>
2022-11-25 15:04:16 +0530

    fix api redirect route

--- functions/api.js
+++ functions/api.js
@@ -6,13 +6,21 @@ const router = express.Router();
 
 router.get('/' , (req , res)=>{
 
-    // res.json({
-    //     'path': 'homepage'
-    // });
-   res.send('hello from simple server :)')
+    res.json({
+        'path': 'homepage'
+    });
 
 })
 
-app.use('/.netlify/functions/api', router);
+router.get('/user' , (req , res)=>{
+
+    res.json({
+        'firstName': 'Manish',
+        'lastName': 'Kumar'
+    });
+
+})
+
+app.use('/', router);
 
 module.exports.handler = serverless(app);
